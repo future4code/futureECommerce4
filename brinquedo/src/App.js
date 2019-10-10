d0e243aca6157454ed339624d23a61cf20580d08
Leonardo <leonardocpn@gmail.com>
2019-10-10 11:50:14 -0300

    inseri o array de brinquedos

--- brinquedo/src/App.js
+++ brinquedo/src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {ItemProduto} from './components/ItemProduto/ItemProduto'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const listaDeBrinquedos = [{
+  id: 1,
+  imagem: 'usaremos uma estatica',
+  nome: '#14 clone trooper',
+  valor: 50,
+  tipo: 'action figure',
+ },
+ {id: 2,
+  imagem: 'usaremos uma estatica',
+  nome: 'Darth Vader',
+  valor: 70,
+  tipo: 'action figure',
+ },
+ {id: 3,
+  imagem: 'usaremos uma estatica',
+  nome: 'TIE Fighter',
+  valor: 100,
+  tipo: 'espaçonave',
+ },
+ {id: 4,
+  imagem: 'usaremos uma estatica',
+  nome: 'BB*',
+  valor: 150,
+  tipo: 'brinquedo de controle remoto',
+ },
+ {id: 5,
+  imagem: 'usaremos uma estatica',
+  nome: 'Crusader',
+  valor: 250,
+  tipo: 'lego',
+ },
+ {id: 6,
+  imagem: 'usaremos uma estatica',
+  nome: 'Storm Strooper',
+  valor: 70,
+  tipo: 'lego',
+ },
+ {id: 7,
+  imagem: 'usaremos uma estatica',
+  nome: 'Espaçonave Rebeldes',
+  valor: 90,
+  tipo: 'lego',
+ },
+ {id: 8,
+  imagem: 'usaremos uma estatica',
+  nome: 'Hailfare Droid',
+  valor: 80,
+  tipo: 'lego',
+ }]
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+      
+    }
+  }
+
+
+  render() {
+    const listaTodosBrinquedos = listaDeBrinquedos.map(cadabrinquedo => {
+      return <ItemProduto ItemProduto={cadabrinquedo} />;
+    });
+   
+    return (
+      <div>
+        {listaTodosBrinquedos}
+        
+          
+        
+        </div>
+      
+    )
+  }
 }
 
-export default App;
+export default App
